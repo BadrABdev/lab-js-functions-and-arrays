37113bfcc7f1cbe5e438b8797410f4f6e7b4bcac
Badr AB <aitbella.b@gmail.com>
2024-11-21 15:47:05 +0100

    solved lab

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,10 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+    if (num1 >= num2) { 
+        return num1; } 
+        else { return num2;}
+
+}
 
 
 
@@ -7,7 +12,14 @@ function maxOfTwoNumbers() {}
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+    if (words.length === 0)
+        return null;
+    let longestWord = words[0];
+    for (let word of words) {
+        if (word.length > longestWord.length) {longestWord=word;}
+    } return longestWord;
+}
 
 
 
@@ -15,7 +27,12 @@ function findLongestWord() {}
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
+    let totalSum = 0;
+    for(let number of numbers) {
+        totalSum+=number; }
+    return totalSum;
+}
 
 
 
@@ -23,7 +40,16 @@ function sumNumbers() {}
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(numbers2) {
+    if (numbers2.length === 0) {
+        return null;
+    }
+    let totalSum = 0;
+    for(let number of numbers2){
+        totalSum+=number;
+    }
+    return totalSum / numbers2.length;
+}
 
 
 
@@ -31,4 +57,17 @@ function averageNumbers() {}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(wordArr,wordToSearch) { if (wordArr.length === 0){
+    console.log(" Nothign here")
+      return null;
+  }
+  for (let i= 0; i < wordArr.length; i +=1){
+      const currentWord = wordArr[i];
+      if (currentWord === wordToSearch){
+          console.log("word is found");
+          return true;
+      }
+  }
+  console.log("NOPE")
+  return false;
+}
